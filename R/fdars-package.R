04571b0778dfa41a7d640c617aee9fe1cd7d4e66
Simon Müller <simon.mueller@muon-stat.com>
2025-12-12 22:47:38 +0100

    Fix R CMD check issues and add vignettes
    - Add imports for stats and utils functions to NAMESPACE
    - Add .data import from ggplot2 for aes()
    - Add fda to Suggests for fdata2fd function
    - Remove compiled libfdars.a from repository
    - Add .Rbuildignore to exclude build artifacts
    - Add 6 vignettes covering all major package features

---
+++ R/fdars-package.R
@@ -0,0 +1,9 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom stats coef dnorm lm median optimize pnorm qnorm quantile rnorm sd var
+#' @importFrom utils head
+#' @importFrom ggplot2 .data
+## usethis namespace: end
+NULL
